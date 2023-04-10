e301cdc4059ce0e1c121661742348864c6fe1449
Veronica <almeida.vrc@gmail.com>
2023-04-10 09:01:49 -0300

    Add getters and setters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Movie myMovie = new Movie();
+        myMovie.setName("E o Vento Levou");
+        myMovie.setReleaseYear(1939);
+        myMovie.setRating(4.8);
+        myMovie.setTotalRatings(146);
+        myMovie.setTimeInMinutes(238);
+        myMovie.setIsIncludedInPlan(true);
+
+        myMovie.showMovieData();
+
+        myMovie.setRating(4.8);
+        myMovie.setRating(10);
+        myMovie.setRating(10);
+        myMovie.setRating(10);
+
+        System.out.println(myMovie.getName());
+        System.out.println(myMovie.getRating());
+        System.out.println(myMovie.getTotalRatings());
+        System.out.println(myMovie.AveragedRatings());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,82 @@
 public class Movie {
-    String name;
-    int releaseYear;
-    boolean includedInPlan;
-    double rating;
-    int totalRatings;
-    int timeInMinutes;
+    private String name; /* atributos */
+    private int releaseYear;
+    private int timeInMinutes;
+    private boolean isIncludedInPlan;
+    private double rating;
+    private int totalRatings;
+
+    void showMovieData() {
+        System.out.println("******************************************");
+
+        System.out.println("Nome do filme: " + name );
+        System.out.println("Ano de Lançamento: " + releaseYear );
+        System.out.println("Duração: " + timeInMinutes );
+        System.out.println("Incluído no Plano: " + isIncludedInPlan );
+        System.out.println("Avaliação: " + rating );
+        System.out.println("Total de Avaliações: " + totalRatings );
+
+        System.out.println("******************************************");
+
+    }
+
+    /* método */
+    void Rate(double score) {
+        this.rating += score;
+        this.totalRatings++;
+    }
+
+    double AveragedRatings(){
+        return ( this.rating / this.totalRatings * 100 );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getTimeInMinutes() {
+        return timeInMinutes;
+    }
+
+    public void setTimeInMinutes(int timeInMinutes) {
+        this.timeInMinutes = timeInMinutes;
+    }
+
+    public boolean isIncludedInPlan() {
+        return isIncludedInPlan;
+    }
+
+    public void setIsIncludedInPlan(boolean isIncludedInPlan) {
+        this.isIncludedInPlan = isIncludedInPlan;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.totalRatings++;
+        this.rating += rating / this.totalRatings;
+
+    }
+
+    public int getTotalRatings() {
+        return totalRatings;
+    }
+
+    public void setTotalRatings(int totalRatings) {
+        this.totalRatings = totalRatings;
+    }
 }
